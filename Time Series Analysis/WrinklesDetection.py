6b02182437ef7443ba8fdde95427a1162254e701
dacliem <dacliem@yahoo.com>
2016-11-09 15:50:51 +0700

    Update WrinklesDetection.py

--- Time Series Analysis/WrinklesDetection.py
+++ Time Series Analysis/WrinklesDetection.py
@@ -14,4 +14,13 @@ fileName = "GDNA_MeasuredValues_ProductE_20160701.csv"
 filePath = os.path.join(pathName, fileName)   
 frame_Measured_E_0701 = pd.read_csv(filePath)
 
-
+============
+import pandas as pd
+import os.path
+  
+pathName = "D:/LiemLD/DSG/Toppan-Gunma/From_Toppan/Sensors_data/GDNA_GS52/Pre-processed/CSV/"
+fileName = "GDNA_SettingValues_E_EN.csv"
+filePath = os.path.join(pathName, fileName)   
+dfSetting = pd.read_csv(filePath)
+dates = pd.to_datetime(dfSetting['S_Timestamp'])
+dfSetting['S_Timestamp'] = dates
